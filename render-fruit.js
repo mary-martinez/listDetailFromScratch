827d3e186dd99a341b59f5144cbfe2c49e651837
Mary Martinez <mary.diana.martinez@gmail.com>
2022-02-08 17:45:22 -0800

    functioning fruit stand

--- render-fruit.js
+++ render-fruit.js
@@ -15,3 +15,46 @@ export function renderFruit(fruit) {
     div.append(img, h2);
     return div;
 }
+
+export function renderFruitDetails(fruit) {
+    const div = document.createElement('div');
+
+    const h1 = document.createElement('h1');
+    h1.classList.add('fruit-name');
+    h1.setAttribute('id', `${fruit.type}-heading`);
+    h1.textContent = `${titleCase(fruit.type)}`;
+
+    const img = document.createElement('img');
+    img.src = `${fruit.img}`;
+    img.setAttribute('id', `${fruit.type}-page-img`);
+    img.classList.add('fruit-page-img');
+
+    const h2 = document.createElement('h2');
+    h2.classList.add('nutrients');
+    h2.textContent = `Nutrient Facts for One ${titleCase(fruit.type)}`;
+
+    const ul = document.createElement('ul');
+    ul.classList.add('nutrient-list');
+
+    const li1 = document.createElement('li');
+    li1.classList.add('calories');
+    li1.textContent = `Calories: ${fruit.nutrition.calories}`;
+
+    const li2 = document.createElement('li');
+    li2.classList.add('sugar');
+    li2.textContent = `Sugar: ${fruit.nutrition.sugar}g`;
+
+    const li3 = document.createElement('li');
+    li3.classList.add('nutrient');
+    li3.textContent = `Source of: ${titleCase(fruit.nutrition.nutrient)}`;
+
+    ul.append(li1, li2, li3);
+
+    div.append(h1, img, h2, ul);
+
+    return (div);
+}
+
+export function titleCase(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
--- test/example.test.js
+++ test/example.test.js
@@ -1,4 +1,4 @@
-import { renderFruit } from '../render-fruit.js';
+import { renderFruit, renderFruitDetails, titleCase } from '../render-fruit.js';
 const test = QUnit.test;
 
 test('renderFruit should return a div that contains an img and an h2', (expect) => {
@@ -16,3 +16,25 @@ test('renderFruit should return a div that contains an img and an h2', (expect) 
 
     expect.equal(actual.outerHTML, expected);
 });
+
+test('titleCase function should take in apple and return Apple', (expect) => {
+    const expected = 'Apple';
+    const actual = titleCase('apple');
+    expect.equal(actual, expected);
+})
+
+test('renderFruitDetails should return a div that contains an h1, an img, an h2, and a ul with 3 li', (expect) => {
+    const expected = `<div><h1 class="fruit-name" id="apple-heading">Apple</h1><img src="../assets/apple.png" id="apple-page-img" class="fruit-page-img"><h2 class="nutrients">Nutrient Facts for One Apple</h2><ul class="nutrient-list"><li class="calories">Calories: 95</li><li class="sugar">Sugar: 19g</li><li class="nutrient">Source of: Fiber</li></ul></div>`;
+
+    const actual = renderFruitDetails({
+        type: 'apple',
+        img: '../assets/apple.png',
+        nutrition: {
+            calories: 95,
+            sugar: 19,
+            nutrient: 'fiber'
+        }
+    });
+
+    expect.equal(actual.outerHTML, expected);
+})
