40000db38066d996950aea0d855bfffd49715a96
Mary Martinez <mary.diana.martinez@gmail.com>
2022-02-08 19:44:21 -0800

    fixing eslint errors

--- fruit/fruit.js
+++ fruit/fruit.js
@@ -1,16 +1,10 @@
 import { fruitArray } from '../fruit-data.js';
-import { renderFruit, renderFruitDetails } from '../render-fruit.js';
+import { renderFruitDetails } from '../render-fruit.js';
 import { findByID } from '../utils.js';
 
 const main = document.querySelector('main');
 
 const params = new URLSearchParams(window.location.search);
-console.log('params', params);
-
 const fruitSelected = findByID(fruitArray, params.get('id'));
-console.log('fruitSelected', fruitSelected);
-
 const fruitDetails = renderFruitDetails(fruitSelected);
-console.log(fruitDetails);
-
 main.append(fruitDetails);
--- test/example.test.js
+++ test/example.test.js
@@ -21,7 +21,7 @@ test('titleCase function should take in apple and return Apple', (expect) => {
     const expected = 'Apple';
     const actual = titleCase('apple');
     expect.equal(actual, expected);
-})
+});
 
 test('renderFruitDetails should return a div that contains an h1, an img, an h2, and a ul with 3 li', (expect) => {
     const expected = `<div class="fruit-details"><h1 class="fruit-name" id="apple-heading">Apple</h1><img src="../assets/apple.png" id="apple-page-img" class="fruit-page-img"><h2 class="nutrients">Nutrient Facts for One Apple</h2><ul class="nutrient-list"><li class="calories">Calories: 95</li><li class="sugar">Sugar: 19g</li><li class="nutrient">Source of: Fiber</li></ul></div>`;
@@ -37,4 +37,4 @@ test('renderFruitDetails should return a div that contains an h1, an img, an h2,
     });
 
     expect.equal(actual.outerHTML, expected);
-})
+});
